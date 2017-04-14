bb8456d5c8121948d4f89aca1bd09b8f34288799
hotsoycandy <wkdwnsghd08@naver.com>
2017-04-15 01:38:11 +0900

    수정
    str_replace 문법 틀린거 고침

--- send.php
+++ send.php
@@ -2,14 +2,14 @@
     include("dbcon.php");
     $text = $_POST['text'];
     $name = $_POST['name'];
-    $text = str_replace($text,"<","&#60");
-    $text = str_replace($text,">","&#62");
-    $text = str_replace($text,"'","&#39");
-    $text = str_replace($text,'"',"&#64");
-    $name = str_replace($name,"<","&#60");
-    $name = str_replace($name,">","&#62");
-    $name = str_replace($name,"'","&#39");
-    $name = str_replace($name,'"',"&#64");
+    $text = str_replace("<","&#60",$text);
+    $text = str_replace(">","&#62",$text);
+    $text = str_replace("'","&#39",$text);
+    $text = str_replace('"',"&#34",$text);
+    $name = str_replace("<","&#60",$name);
+    $name = str_replace(">","&#62",$name);
+    $name = str_replace("'","&#39",$name);
+    $name = str_replace('"',"&#34",$name);
     $sql = "INSERT INTO chat SET text='{$text}', name='{$name}', wdate=now()";
     $db->query($sql);
 ?>
