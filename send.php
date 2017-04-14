47541e9a3f9953a664b2f1f92da0004636bb9f4e
hotsoycandy <wkdwnsghd08@naver.com>
2017-04-15 01:27:11 +0900

    특수문자, 태그 막음
    str_replace 활용

--- send.php
+++ send.php
@@ -2,6 +2,14 @@
     include("dbcon.php");
     $text = $_POST['text'];
     $name = $_POST['name'];
+    $text = str_replace($text,"<","&#60");
+    $text = str_replace($text,">","&#62");
+    $text = str_replace($text,"'","&#39");
+    $text = str_replace($text,'"',"&#64");
+    $name = str_replace($name,"<","&#60");
+    $name = str_replace($name,">","&#62");
+    $name = str_replace($name,"'","&#39");
+    $name = str_replace($name,'"',"&#64");
     $sql = "INSERT INTO chat SET text='{$text}', name='{$name}', wdate=now()";
     $db->query($sql);
 ?>
